e9e5cb9eb3f9f5ff58bae3ba48014380b5765727
Artem LD31D <artem.12m21@gmail.com>
2021-02-13 19:14:22 +0200

    Added handler to sending photo

--- bot/handlers/send_message.py
+++ bot/handlers/send_message.py
@@ -13,3 +13,13 @@ async def send_text_message_handler(message: types.Message):
 	companion_id = await redis.get_user_connection(user_id)
 
 	await bot.send_message(companion_id, text)
+
+
+@dp.message_handler(IsUserConnectionExist(), content_types=types.ContentType.PHOTO)
+async def send_photo_message_handler(message: types.Message):
+	user_id = message.chat.id
+	photo = message.photo[-1].file_id
+
+	companion_id = await redis.get_user_connection(user_id)
+
+	await bot.send_photo(companion_id, photo)
