fe503fcbe0d1c5f4df2687cabdc4a0246ae80f6d
Artem LD31D <artem.12m21@gmail.com>
2021-02-14 12:02:04 +0200

    Added webhook

--- bot/config.py
+++ bot/config.py
@@ -1,12 +1,18 @@
 from os import getenv
 
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 REDIS_HOST = getenv("REDIS_HOST")
 REDIS_PASSWORD = getenv("REDIS_PASSWORD") if getenv("REDIS_PASSWORD") else None
+
+HEROKU_APP_NAME = getenv('HEROKU_APP_NAME')
+
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = 8080
--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from aiogram import executor
+
+from bot.loader import bot
 from bot.handlers import dp
+from bot.config import WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT, WEBHOOK_URL
+
+
+async def on_startup(dp):
+	await db.create()
+	await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
 
 
 if __name__ == '__main__':
 	executor.start_polling(dp)
+
+	executor.start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=False,
+        on_startup=on_startup,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
